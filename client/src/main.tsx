1cf7c8b4044968078dc2bf83365fbe08a93febe3
Prateek Sharma <prateeks.iu@gmail.com>
2025-09-13 18:16:03 -0700

    remove basename

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,4 +4,4 @@ import './index.css'
 import { HashRouter } from "react-router-dom";
 
 
-createRoot(document.getElementById("root")!).render(<HashRouter basename="/genre_prediction"><App /></HashRouter>);
+createRoot(document.getElementById("root")!).render(<HashRouter><App /></HashRouter>);
