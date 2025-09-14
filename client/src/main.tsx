5799a8c406384478d9a58e41764ce50f56a5f55c
Prateek Sharma <prateeks.iu@gmail.com>
2025-09-13 18:03:59 -0700

    Fix: remove nested BrowserRouter; use single HashRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 
-createRoot(document.getElementById("root")!).render(<BrowserRouter basename="/genre_prediction"><App /></BrowserRouter>);
+createRoot(document.getElementById("root")!).render(<HashRouter basename="/genre_prediction"><App /></HashRouter>);
